e28dc4fd49925447c697b7edb992150448d17801
momen <momen@MILF_HUNTER>
2024-03-12 21:05:34 +0200

    Добавьте файлы проекта.

---
+++ hw 12/hw 12.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double num1 = 5;
+    double num2 = 10;
+
+    double* pn = &num1;
+    double* pr = &num2;
+    cout << "Lenght in variables = " << *pr - *pn << "\n";
+    cout << "Lenght in bytes = " << (*pr - *pn) * 8;
+}
